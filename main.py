3921e9e5e752e4d6cfa8012b4a3081ef3ceddb86
zindarr <115018856+zindarr@users.noreply.github.com>
2023-06-11 08:30:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ async def predict_api(file: UploadFile = File(...)):
 
 if name == "main":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
