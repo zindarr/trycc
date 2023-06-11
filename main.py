0b0090bd20a7be4210901a973d31d6020049f305
zindarr <115018856+zindarr@users.noreply.github.com>
2023-06-11 08:20:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ async def predict_api(file: UploadFile = File(...)):
 
     prediction = predict_image_class(image)
     return {'prediction': prediction}
+
+if name == "main":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
